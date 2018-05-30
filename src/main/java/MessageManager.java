e8ece4bb13b6ae275974c50f1f0e85c61a040fe4
diegobeckertw <dbecker@thoughtworks.com>
2018-05-30 16:59:34 -0300

    Main Menu

--- src/main/java/MessageManager.java
+++ src/main/java/MessageManager.java
@@ -2,4 +2,6 @@ public class MessageManager {
 
     public static final String WELCOME_MESSAGE = "Welcome Biblioteca";
 
+    public static final String EXCEPTION_MESSAGE = "Ocorreu um erro, escolha outra opção";
+
 }
