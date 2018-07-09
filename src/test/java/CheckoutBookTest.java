345f58b9670b4c492b79a7c45a2858d8c6e7f00a
diegobeckertw <dbecker@thoughtworks.com>
2018-07-09 12:10:29 -0300

    Checkout Book

--- src/test/java/CheckoutBookTest.java
+++ src/test/java/CheckoutBookTest.java
@@ -11,4 +11,18 @@ public class CheckoutBookTest {
 
         MatcherAssert.assertThat(checkoutBook.validateBookOption(1, listBook), Is.is(true));
     }
+
+    @Test
+    public void returnTrueWhenListBookAreDiferents() {
+        CheckoutBook checkoutBook = new CheckoutBook();
+
+        ListBook listBook = new ListBook();
+
+        Book mockbook = listBook.findBookByCode(1);
+
+        checkoutBook.checkout(mockbook.getCode(), listBook);
+
+        MatcherAssert.assertThat(mockbook.isAvailable(), Is.is(false));
+
+    }
 }
